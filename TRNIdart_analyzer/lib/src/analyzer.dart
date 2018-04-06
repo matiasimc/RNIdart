0a9ff9d2add79391f453ae59c1d8a30592ebbf83
Matías Meneses <matias.imc@gmail.com>
2018-04-06 13:58:00 -0300

    Start refactoring of TRNIdart_analyzer package for plugin support

---
+++ TRNIdart_analyzer/lib/src/analyzer.dart
@@ -0,0 +1,31 @@
+import 'package:analyzer/analyzer.dart' show AnalysisError, CompilationUnit;
+import 'dart:io' as io show File;
+import 'package:path/path.dart' as pathos;
+import 'package:analyzer/src/generated/engine.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:analyzer/error/error.dart';
+import 'package:analyzer/file_system/file_system.dart';
+import 'package:analyzer/file_system/memory_file_system.dart';
+import 'package:analyzer/source/package_map_resolver.dart';
+import 'package:analyzer/src/generated/sdk.dart';
+import 'package:analyzer/task/dart.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:analyzer/file_system/physical_file_system.dart';
+import 'package:analyzer/src/dart/sdk/sdk.dart';
+import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/src/generated/source_io.dart';
+import 'package:analyzer/src/source/source_resource.dart';
+import 'package:cli_util/cli_util.dart' show getSdkPath;
+
+class TRNIAnalyzer {
+  MemoryResourceProvider resourceProvider = new MemoryResourceProvider();
+  DartSdk sdk;
+  AnalysisContext context;
+
+  TRNIAnalyzer() {
+    sdk = new FolderBasedDartSdk(
+        resourceProvider, resourceProvider.getFolder(getSdkPath()));
+    context = AnalysisEngine.instance.createAnalysisContext();
+  }
+
+}
---
+++ TRNIdart_analyzer/lib/src/errors/errors.dart
@@ -0,0 +1,37 @@
+import 'package:analyzer/error/error.dart';
+
+class UndefinedInterface implements ErrorCode {
+  final String name = "Undefined interface";
+  String message;
+  final String correction;
+
+  UndefinedInterface(String element, [String this.correction]) {
+    this.message = "Undefined interface for element ${element}";
+  }
+
+  @override
+  ErrorSeverity get errorSeverity => ErrorSeverity.ERROR;
+
+  @override
+  ErrorType get type => ErrorType.STATIC_WARNING;
+
+  @override
+  String get uniqueName => this.name;
+}
+
+class DummyError implements ErrorCode {
+  final String name = "Dummy Error";
+  final String message = "This plugin works!";
+  final String correction;
+
+  DummyError([String this.correction]);
+
+  @override
+  ErrorSeverity get errorSeverity => ErrorSeverity.ERROR;
+
+  @override
+  ErrorType get type => ErrorType.STATIC_WARNING;
+
+  @override
+  String get uniqueName => this.name;
+}
